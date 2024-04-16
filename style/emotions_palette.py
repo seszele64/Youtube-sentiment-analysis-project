500a5706fb1dcd9bd9c1aebf7c1563bb94b51174
seszele64 <76787567+seszele64@users.noreply.github.com>
2024-04-16 18:50:59 +0200

    new file + removing some bloat code

---
+++ style/emotions_palette.py
@@ -0,0 +1,9 @@
+palette = {
+    'red': '#E63946',
+    'green': '#2A9D8F',
+    'black': '#343A40',
+    'yellow': '#F4A261',
+    'grey': '#A8A8A8',
+    'blue': '#274754',
+    'orange': '#F4A261'
+}
