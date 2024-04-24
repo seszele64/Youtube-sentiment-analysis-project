5a6b26f5e9c69a3c06379aaccf3e7767e3d1eb9d
seszele64 <76787567+seszele64@users.noreply.github.com>
2024-04-24 12:31:42 +0200

    fixed overlapping color that i had overlooked

--- style/emotions_palette.py
+++ style/emotions_palette.py
@@ -2,7 +2,7 @@ palette = {
     'red': '#E63946',
     'green': '#2A9D8F',
     'black': '#343A40',
-    'yellow': '#F4A261',
+    'yellow': '#efff63',
     'grey': '#A8A8A8',
     'blue': '#274754',
     'orange': '#F4A261'
